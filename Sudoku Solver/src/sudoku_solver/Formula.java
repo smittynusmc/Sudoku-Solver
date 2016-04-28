e5a98c93a6b2c27ac7f5f1a284f27af74f597556
Adam Tucker <smittynusmc@gmail.com>
2016-04-28 12:37:22 -0400

    Removed old test methods, removed methods from formula that are not need for solver

--- Sudoku Solver/src/sudoku_solver/Formula.java
+++ Sudoku Solver/src/sudoku_solver/Formula.java
@@ -52,12 +52,6 @@ public class Formula {
 	// list of clauses in CNF 
 	private List<Clause> formulaList;
 
-	// The current partial or completed state for this formula
-	private List <Literal> successState;
-
-	//index for backtracking
-	int lastIndex;
-
 	//for return value test  
 	boolean isEmpty;
 
@@ -84,7 +78,6 @@ public class Formula {
 	public Formula()
 	{
 		formulaList = new ArrayList<Clause>(13000);
-		successState = new ArrayList <Literal> (1000);
 		isEmpty = true; 
 		numVariables = 0;
 		minClauseSize=Integer.MAX_VALUE;
@@ -217,174 +210,7 @@ public class Formula {
 		return formulaList.size();
 	}
 
-	/**
-	 * Gets the last index
-	 * @return The last index
-	 */
-	public int getLastIndex() {
-		return lastIndex;
-	}
 
-	/**
-	 * Sets the last index
-	 * @param lastIndex The integer last index will be set as
-	 */
-	public void setLastIndex(int lastIndex) {
-		this.lastIndex = lastIndex;
-	}
-
-	/**
-	 * Gets the success state
-	 * @return The success state
-	 */
-	public List<Literal> getSuccessState() {
-		Collections.sort(successState);
-		return successState;
-	}
-
-	/**
-	 * Recursive backtracking call to solve the formula
-	 * @return True if the formula is satisfiable and false otherwise
-	 */
-	public boolean runSolver()
-	{
-		//Base case if empty formula is satisfiable
-		if(this.isEmpty())
-		{
-			return true;			
-		}
-		//Base case 2 if there is empty clause backtrack through the tree
-		//to try different solutions
-		else if(this.hasEmptyClause)
-		{
-			return false;
-		}
-
-		//Recursive element
-		else{
-			// Creates a child formula with true (a formula that has evaluated classes on the
-			// current level, removed satisfied clauses and removes unnecessary literals
-			Formula testTrue = createChild(true);
-			if(testTrue.runSolver())
-			{	
-				//Found a solution and adds to successState
-				successState.addAll(testTrue.getSuccessState());
-				successState.add(new Literal(lastIndex+1,true));
-				return true;
-			}
-			// Creates a child formula with true (a formula that has evaluated classes on the
-			// current level, removed satisfied clauses and removes unnecessary literals
-			Formula testFalse = createChild(false);
-			if(testFalse.runSolver())
-			{	
-				// Found a solution and adds to successState
-				successState.addAll(testFalse.getSuccessState());
-				successState.add(new Literal(lastIndex+1,false));
-				return true;
-			}
-			else 
-			{
-				return false;
-			}
-		}
-	}
-
-	/**
-	 * Creates a child formula to be tested by the solver
-	 * As the solver recursively travels through the collection
-	 * each new child will have a new formula evaluated by the create child method
-	 * @param givenBoolean The value to evaluate with the current formula
-	 * @return The new formula that has been evaluated at the current level
-	 * with any satisfied clause removed and unnecessary literal removed
-	 */
-	public Formula createChild(boolean givenBoolean)
-	{
-		//new formula with modified formula
-		Formula child= new Formula();
-		child.setLastIndex(this.lastIndex+1);
-		//Creating Literal based on index# and value given. Using lastIndex until 
-		//another option becomes available.
-		Literal testLiteral = new Literal(this.lastIndex+1,givenBoolean);
-		//iterator to evaluate each clause in Formula
-		Iterator <Clause> itty = this.formulaList.iterator();
-		//current working Clause
-		Clause testClause; 
-		//modified clause for child
-		Clause resultClause;
-
-		//loop to evaluate each Clause
-		while(itty.hasNext())
-		{
-			testClause=itty.next();
-			// Sends the clause to be evaluated
-			resultClause=testClause.evaluateClause(testLiteral);
-			// If returnClause is null the clause was satisfied
-			// otherwise add the evaluate clause to the child formula
-			if(resultClause!=null)
-			{
-				child.addClause(resultClause);
-			}
-
-		}
-		return child;
-	}
-
-	/**
-	 * This method loads and returns a formula from a file specification
-	 * @param filename Name of the file containing the formula
-	 * @return The parsed Formula
-	 */
-	public static Formula readFromFile(String filename)
-	{
-		BufferedReader br = null;
-		String line = null;
-		@SuppressWarnings("unused")
-		int numVars, numClauses, var;
-		Formula f = new Formula();
-		try {
-			br = new BufferedReader(new FileReader(filename));
-			while((line = br.readLine()) != null){          
-				if(line.startsWith("c"))
-					continue;
-				if(line.length() == 0)
-					continue;
-				if(line.startsWith("p cnf")){
-					line = line.replaceAll("\\s+", " ");
-					String[] data = line.split("[ \t]");
-					numVars = Integer.parseInt(data[2]);
-					numClauses = Integer.parseInt(data[3]);
-					f.setNumVariables(numVars);
-					//f.setNumClauses(numClauses);
-				}
-				else{
-					Clause c = new Clause();
-					while (!(line.contains(" 0"))) {
-						line = line + br.readLine();
-					}
-					Scanner sc = new Scanner(line);
-					while(sc.hasNextInt()){
-						var = sc.nextInt();
-						if(var == 0)
-							break;
-						else if(var > 0){
-							c.add(new Literal(var));
-						}  
-						else{
-							c.add(new Literal(var));
-						}
-					}
-					f.addClause(c);
-					sc.close();
-				}
-			}
-			br.close();
-		} catch (FileNotFoundException ex) {
-			System.out.println("file I/O error: " + ex);
-		} catch (IOException ex) {
-			System.out.println("file I/O error: " + ex);
-		}
-		return f;
-	}
 
 	/**
 	 * Takes a formula and makes a new cnf file 
@@ -403,7 +229,7 @@ public class Formula {
 			}
 			fileWriter.close();
 		} catch (IOException e) {
-			System.err.println("Problem writing to the file statsTest.txt");
+			System.err.println("Problem writing to the file " + nameOfSudoku);
 		}
 	}
 
--- Sudoku Solver/src/sudoku_solver/FormulaTest.java
+++ Sudoku Solver/src/sudoku_solver/FormulaTest.java
@@ -1,17 +1,18 @@
 package sudoku_solver;
 
+import java.io.BufferedReader;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 import org.junit.Test;
 
-import org.sat4j.core.*;
-import org.sat4j.minisat.SolverFactory;
-import org.sat4j.specs.ISolver;
 
 /**
  * Test a suite for a formula
@@ -25,33 +26,77 @@ import org.sat4j.specs.ISolver;
 
 public class FormulaTest {
 	
-	/**
-	 * Test the creation of a empty formula
-	 */
-	@Test
-	public void testFormula() {
-
-		
-		Formula testForm = new Formula();
-		assertTrue(testForm.isEmpty());
-		
-		assertEquals(testForm.getLastIndex(),0);
-			
-	}
+    
+        public static void main (String[]args) {
+            FormulaTest form = new FormulaTest ();
+            form.testWritetoFile();
+        }
 	
 	/**
+	 * The writetoFile method in formula takes in this case a txt file and uses the formulaList (instance of
+         * Sudoku) and writes that instance it a newly created cnf file
 	 * Takes a cnf .txt file and makes a formula and then takes that
-	 * formula and makes a .cnf file
+	 * formula and makes a .cnf file. The result the same s28.cnf file minus some of the original comments
 	 */
+
 	@Test
 	public void testWritetoFile () {
-		File file = new File("C:/Users/Adam Tucker/Google Drive/School/Spring 2016/DAA/sat4j-sat4j-sat-v20130419/s28.cnf");
-		Formula formula = Formula.readFromFile(file.toString());
+		File file = new File("C:/Users/Owner/Dropbox/group/Brute Force SAT solver project/Brute Force SAT solver project/s20 - copy.cnf");
+		Formula formula = readFromFile(file.toString());
 		formula.writeToFile(file,"NAME OF FILE");
-		//String[] array = null;
-		//array[0] = file.toString();
-		//ISolver solver = SolverFactory.newDefault();
-		//solver.
+	}
+        
+        /**
+	 * This method loads and returns a formula from a file specification
+	 * @param filename name of the file containing the formula
+	 * @return the Formula
+	 */
+	public Formula readFromFile(String filename)
+	{
+		BufferedReader br = null;
+		String line = null;
+		int numVars, numClauses, var;
+		Formula f = new Formula();
+		try {
+			br = new BufferedReader(new FileReader(filename));
+			while((line = br.readLine()) != null){          
+				if(line.startsWith("c"))
+					continue;
+				if(line.length() == 0)
+					continue;
+				if(line.startsWith("p cnf")){
+					line = line.replaceAll("\\s+", " ");
+					String[] data = line.split("[ \t]");
+					numVars = Integer.parseInt(data[2]);
+					numClauses = Integer.parseInt(data[3]);
+					f.setNumVariables(numVars);
+					//f.setNumClauses(numClauses);
+				}
+				else{
+					Scanner sc = new Scanner(line);
+					Clause c = new Clause();
+					while(sc.hasNextInt()){
+						var = sc.nextInt();
+						if(var == 0)
+							break;
+						else if(var > 0){
+							c.add(new Literal(var));
+						}  
+						else{
+							c.add(new Literal(var));
+						}
+					}
+					f.addClause(c);
+					sc.close();
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("file I/O error: " + ex);
+		} catch (IOException ex) {
+			System.out.println("file I/O error: " + ex);
+		}
+		return f;
 	}
 	
 	/**
@@ -118,142 +163,5 @@ public class FormulaTest {
 		assertFalse(testForm.isEmpty());
 	}
 	
-	/**
-	 * Test retrieving the number of clauses in a formula
-	 */
-	@Test
-	public void testGetNumClauses() {
-		Clause clause1 = new Clause();
-		Clause clause2 = new Clause();
-		Clause clause3 = new Clause();
-		
-		Literal onet = new Literal(1);
-		Literal onef = new Literal(-1);
-		Literal twot = new Literal(2);
-		Literal twof = new Literal(-2);
-		Literal threet = new Literal(3);
-		Literal threef = new Literal(-3);
-	
-		clause1.add(onet);
-		clause1.add(twot);
-		
-		clause2.add(onef);
-		clause2.add(twot);
-		clause2.add(threet);
-		
-		clause3.add(onet);
-		clause3.add(twof);
-		clause3.add(threef);
-		
-		Formula testForm = new Formula();
-		
-		testForm.addClause(clause1);
-		assertEquals(testForm.getNumClauses(),1);
-		testForm.addClause(clause2);
-		assertEquals(testForm.getNumClauses(),2);
-		testForm.addClause(clause3);
-		assertEquals(testForm.getNumClauses(),3);
-		Clause clause4=new Clause();
-		testForm.addClause(clause4);
-		assertEquals(testForm.getNumClauses(),4);
-		
-		Formula child1t = testForm.createChild(true);
-		Formula child1f = testForm.createChild(false);
-		assertEquals(child1t.getNumClauses(),2);
-		assertEquals(child1f.getNumClauses(),3);
-		
-	}
-	
-	/**
-	 * Test retrieving the last index of formula
-	 */
-	@Test
-	public void testGetLastIndex() {
-		Clause clause1 = new Clause();
-		Clause clause2 = new Clause();
-		Clause clause3 = new Clause();
-		
-		Literal onet = new Literal(1);
-		Literal onef = new Literal(-1);
-		Literal twot = new Literal(2);
-		Literal twof = new Literal(-2);
-		Literal threet = new Literal(3);
-		Literal threef = new Literal(-3);
-	
-		clause1.add(onet);
-		clause1.add(twot);
-		
-		clause2.add(onef);
-		clause2.add(twot);
-		clause2.add(threet);
-		
-		clause3.add(onet);
-		clause3.add(twof);
-		clause3.add(threef);
-		
-		Formula testForm = new Formula();
-		
-		testForm.addClause(clause1);
-		testForm.addClause(clause2);
-		testForm.addClause(clause3);
-		
-		assertEquals(testForm.getLastIndex(),0);
-		Formula child1t = testForm.createChild(true);
-		Formula child1f = testForm.createChild(false);
-		assertEquals(child1t.getLastIndex(),1);
-		assertEquals(child1f.getLastIndex(),1);
-		
-	}
-	
-	/**
-	 * Test the recursive solver and checks to see if successState matches
-	 */
-	@Test
-	public void testRunSolver() {
-		
-		Clause clause1 = new Clause();
-		Clause clause2 = new Clause();
-		Clause clause3 = new Clause();
-		
-		Clause clause5 = new Clause();
-		Clause clause6 = new Clause();
-		Clause clause7 = new Clause();
-		
-		Literal onet = new Literal(1);
-		Literal onef = new Literal(-1);
-		Literal twot = new Literal(2);
-		Literal twof = new Literal(-2);
-		Literal threet = new Literal(3);
-		Literal threef = new Literal(-3);
-	
-		clause1.add(onet);
-		clause1.add(twot);
-		
-		clause2.add(onef);
-		clause2.add(twot);
-		clause2.add(threet);
-		
-		clause3.add(onet);
-		clause3.add(twof);
-		clause3.add(threef);
-		
-		Formula testForm = new Formula();
-		testForm.addClause(clause1);
-		testForm.addClause(clause2);
-		testForm.addClause(clause3);
-		Clause clause4=new Clause();
-		testForm.addClause(clause4);
-		assertFalse(testForm.runSolver());
-		
-		Formula test2 = new Formula();
-		clause7.add(onet);
-		clause5.add(twot);
-		clause6.add(threet);
-		test2.addClause(clause5);
-		test2.addClause(clause6);
-		test2.addClause(clause7);
-		assertTrue(test2.runSolver());
-		assertEquals(test2.getSuccessState().toString(),"[1 true, 2 true, 3 true]");
-	}
 
 }
